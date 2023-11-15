cddc64714a7d163a7cba7555db8634db1c0da758
marcusut <144730253+marcusut@users.noreply.github.com>
2023-11-16 00:51:56 +0200

    jah

--- mang.py
+++ mang.py
@@ -75,13 +75,14 @@ def new_game():
     katsed = 7
     sõna = sõnad[random.randint(0, len(sõnad)-1)]
     arvatud = []
+    valed = []
     while katsed > 0:
         lüngad = "".join([täht if täht in arvatud else '_' for täht in sõna])
         joonista(katsed)
         print(f"Sõna: {lüngad}")
+        print(f"Valed arvamised: {valed}")
         print(f"Valesid katseid järel: {katsed}")
         arvamus = input("Paku täht: ").casefold()
-
         if arvamus in sõna:
             arvatud.append(arvamus)
             if all(täht in arvatud for täht in sõna):
@@ -89,6 +90,7 @@ def new_game():
                 return
         else:
             katsed -= 1
+            valed.append(arvamus)
 
     print(f"Mäng läbi! Sõna oli: {sõna}")
 
