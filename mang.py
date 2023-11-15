d78569e3c9d00290f7ca2e9c0c7c43f2de30dfb4
marcusut <144730253+marcusut@users.noreply.github.com>
2023-11-16 00:34:21 +0200

    alfaversioon valmis I guess

--- mang.py
+++ mang.py
@@ -2,10 +2,94 @@ import random
 
 with open("poomismang\lemmad.txt", encoding="ANSI") as f:
     sõnad =  f.read().split('\n')
+def joonista(katsed):
+    faasid = [
+        """
+           -----
+               |
+               |
+               |
+               |
+               |
+        """,
+        """
+           -----
+               |
+           O   |
+               |
+               |
+               |
+        """,
+        """
+           -----
+               |
+           O   |
+           |   |
+               |
+               |
+        """,
+        """
+           -----
+               |
+           O   |
+          /|   |
+               |
+               |
+        """,
+        """
+           -----
+               |
+           O   |
+          /|\  |
+               |
+               |
+        """,
+        """
+           -----
+               |
+           O   |
+          /|\  |
+          /    |
+               |
+        """,
+        """
+           -----
+               |
+           O   |
+          /|\  |
+          / \  |
+               |
+        """,
+        """
+           -----
+           |   |
+           O   |
+          /|\  |
+          / \  |
+               |
+        """
+    ]
+    print(faasid[7-katsed])
 
 def new_game():
+    katsed = 7
     sõna = sõnad[random.randint(0, len(sõnad)-1)]
-    lüngad = " _ " * len(sõna)
-    print(lüngad)
+    arvatud = []
+    while katsed > 0:
+        lüngad = "".join([täht if täht in arvatud else '_' for täht in sõna])
+        joonista(katsed)
+        print(f"Sõna: {lüngad}")
+        print(f"Valesid katseid järel: {katsed}")
+        arvamus = input("Paku täht: ").casefold()
+
+        if arvamus in sõna:
+            arvatud.append(arvamus)
+            if all(täht in arvatud for täht in sõna):
+                print(f"Palju õnne! Sa arvasid sõna õieti ära. Sõna oli: {sõna}")
+                return
+        else:
+            katsed -= 1
+
+    print(f"Mäng läbi! Sõna oli: {sõna}")
 
 new_game()
